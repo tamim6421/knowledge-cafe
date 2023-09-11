eedc232710c2f6a9cf047152bbc795e61bbd569f
ProgrammingHero <tamimhossain6421@gmail.com>
2023-09-11 19:16:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl text-center my-20 bg-red-400'>Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
